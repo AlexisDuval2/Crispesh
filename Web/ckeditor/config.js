8b04996863b155c834c1ce307c36c7ae148f987b
Duval Alexis <1806165@decinfo.cvm>
2019-05-09 13:05:47 -0400

    ckeditor un peu mieux (taille, couleur, boutons, etc)

--- Web/ckeditor/config.js
+++ Web/ckeditor/config.js
@@ -4,44 +4,34 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+//----------------------------------------------------------------------------------------
+// configurations par Alexis
+//----------------------------------------------------------------------------------------
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
+CKEDITOR.editorConfig = function( config ) {
 	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others', groups: [ 'others' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'about', groups: [ 'about' ] }
 	];
 
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
-
-	// Set the most common block elements.
+	config.removeButtons = 'Subscript,Superscript,PasteText,PasteFromWord,Scayt,Anchor,Table,SpecialChar,Source,RemoveFormat,Strike,Outdent,Indent,Styles,About';
 	config.format_tags = 'p;h1;h2;h3;pre';
-
-	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
-	//----------------------------------------------------------------------------------------
-	// configurations par Alexis
-	//----------------------------------------------------------------------------------------
-	config.width = 500;
-	config.height = 100;
+	config.width = "100%";
+	config.height = "100%";
 	config.uiColor = "#0ea8dd";
-	//----------------------------------------------------------------------------------------
 };
+
+//----------------------------------------------------------------------------------------

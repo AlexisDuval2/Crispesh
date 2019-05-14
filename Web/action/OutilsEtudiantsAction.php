206d370f49c8fdea25e2f2761fbc603576914acf
Duval Alexis <1806165@decinfo.cvm>
2019-05-14 16:50:42 -0400

    ajout des fichiers Action dans le dossier action

---
+++ Web/action/OutilsEtudiantsAction.php
@@ -0,0 +1,154 @@
+
+<?php
+	require_once("action/OutilsEtudiantsAction.php");
+	$action = new OutilsEtudiantsAction();
+	$action->execute();
+	require_once("partial/header.php");
+?>
+
+			<!-- CONTENT -->
+			<div id="content">
+				<div id="page-header" class="style-1">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-6">
+
+								<h4>Intégrer les nouvelles populations étudiantes en situation de handicap aux études supérieures : mission possible (2011-2013)</h4>
+
+							</div><!-- col -->
+							<div class="col-sm-6">
+								<ol class="breadcrumb">
+									<li>
+										<a href="projet_integrer_etudiants.php">Comité INTERORDRES</a>
+									</li>
+									<li class="active">Outils pour les étudiants</li>
+								</ol>
+							</div><!-- col -->
+						</div><!-- row -->
+					</div><!-- container -->
+				</div><!-- page-header -->
+
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="widget widget-categories">
+								<img src="images/projets/interordres.jpg" alt="Intégrer les nouvelles populations étudiantes en situation de handicap aux études supérieures : mission possible (2011-2013)" />
+								<ul>
+									<li>
+										<a href="outils_generaux.php">Outils généraux</a>
+									</li>
+									<li class="active">
+										<a href="javascript:;">Outils pour les étudiants</a>
+									</li>
+									<li>
+										<a href="outils_intervenants.php">Outils pour les intervenants</a>
+									</li>
+									<li>
+										<a href="outils_enseignants.php">Outils pour les enseignants</a>
+									</li>
+									<li>
+										<a href="outils_etablissements.php">Outils pour les établissements</a>
+									</li>
+								</ul>
+							</div><!-- widget-categories -->
+						</div>
+						<div class="col-sm-8">
+							<div class="col-sm-12 wrap_liens_outils">
+								<p>
+									<a href="pdf/outils_etudiants/cegeps_universites_ressemblances_differences.pdf" title="Cégeps et universités: ressemblances et différences" target="_blank">
+										<img src="images/projets/pdf.png" /> Cégeps et universités: ressemblances et différences
+									</a>
+								</p>
+								<p>
+									<a href="pdf/outils_etudiants/la_demarche_d_orientation.pdf" title="La démarche d'orientation" target="_blank">
+										<img src="images/projets/pdf.png" /> La démarche d'orientation
+									</a>
+								</p>
+								<p>
+									<a href="pdf/outils_etudiants/la_transition_scolaire.pdf" title="La transition scolaire" target="_blank">
+										<img src="images/projets/pdf.png" /> La transition scolaire
+									</a>
+								</p>
+								<p>
+									<a href="pdf/outils_etudiants/le_metier_d_etudiant.pdf" title="Le métier d'étudiant" target="_blank">
+										<img src="images/projets/pdf.png" /> Le métier d'étudiant
+									</a>
+								</p>
+								<p>
+									<a href="pdf/outils_etudiants/l_importance_de_votre_evaluation.pdf" title="L'importance de votre évaluation" target="_blank">
+										<img src="images/projets/pdf.png" /> L'importance de votre évaluation
+									</a>
+								</p>
+								<p>
+									<a href="pdf/outils_etudiants/strategies_pour_vous_aider_a_sauver_votre_session.pdf" title="Stratégies pour vous aider à sauver votre session" target="_blank">
+										<img src="images/projets/pdf.png" /> Stratégies pour vous aider à sauver votre session
+									</a>
+								</p>
+								<hr>
+							</div>
+							<div class="col-sm-12">
+								<h4>Troubles d'apprentissage - Evelyne</h4>
+								<div class="sites-embed-content sites-embed-type-youtube">
+									<iframe title="Troubles d'apprentissage - Evelyne"
+											src="https://www.youtube.com/embed/dg3fki_Fi3g?rel=0"
+											allowfullscreen="true"
+											width="480"
+											height="270"
+											frameborder="0">
+									</iframe>
+								</div>
+								<hr>
+								<h4>Troubles du déficit de l'attention - Francis, Noémie </h4>
+								<div class="sites-embed-content sites-embed-type-youtube">
+									<iframe title="Troubles du déficit de l'attention - Francis, Noémie"
+											src="https://www.youtube.com/embed/wGLT0wC2ZLU?rel=0"
+											allowfullscreen="true"
+											width="480"
+											height="270"
+											frameborder="0">
+									</iframe>
+								</div>
+								<hr>
+								<h4>Trouble envahissant du développement: Louis-Phillipe</h4>
+								<div class="sites-embed-content sites-embed-type-youtube">
+									<iframe title="Trouble envahissant du développement: Louis-Phillipe"
+											src="https://www.youtube.com/embed/9rMdDf4er0E?rel=0"
+											allowfullscreen="true"
+											width="480"
+											height="270"
+											frameborder="0">
+									</iframe>
+								</div>
+								<hr>
+								<h4>Troubles de santé mentale: Marianne</h4>
+								<div class="sites-embed-content sites-embed-type-youtube">
+									<iframe title="Troubles de santé mentale: Marianne"
+											src="https://www.youtube.com/embed/bMLUuuo81D8?rel=0"
+											allowfullscreen="true"
+											width="480"
+											height="270"
+											frameborder="0">
+									</iframe>
+								</div>
+								<hr>
+								<h4>Service d'intégration aux étudiants en situation de handicap: Mathieu&nbsp;</h4>
+								<div class="sites-embed-content sites-embed-type-youtube">
+									<iframe title="Service d'intégration aux étudiants en situation de handicap: Mathieu"
+											src="https://www.youtube.com/embed/klYDF_WNO0Q?rel=0"
+											allowfullscreen="true"
+											width="480"
+											height="270"
+											frameborder="0">
+									</iframe>
+								</div>
+							</div>
+							<div class="col-sm-12">
+								<hr>
+								<a href="nos_projets.php" class="btn btn-blue btn_retour_projet">Retour à la liste des projets</a>
+							</div>
+						</div><!-- col -->
+					</div><!-- row -->
+				</div><!-- container -->
+
+<?php
+	require_once("partial/footer.php");

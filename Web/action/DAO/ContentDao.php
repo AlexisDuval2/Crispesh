36f8793dd0877530a16c1ff0bca4e30e7b624ebb
Duval Alexis <1806165@decinfo.cvm>
2019-05-15 18:44:04 -0400

    modification du texte dans l'accueil fonctionne

--- Web/action/DAO/ContentDao.php
+++ Web/action/DAO/ContentDao.php
@@ -2,13 +2,34 @@
 <?php
 	class ContentDao {
 
-		private static $chemin = "action/DAO/indexData.txt";
+		//-------------------------------------------------
+		// PAGE INDEX
+		//-------------------------------------------------
+		// titre
+		private static $indexCheminTitre = "action/DAO/indexTitreData.txt";
 
-		public static function lire() {
-			return file_get_contents(ContentDao::$chemin);
+		public static function lireTitreIndex() {
+			return file_get_contents(ContentDao::$indexCheminTitre);
 		}
 
-		public static function ecrire($message) {
-			file_put_contents(ContentDao::$chemin, $message);
+		public static function ecrireTitreIndex($message) {
+			file_put_contents(ContentDao::$indexCheminTitre, $message);
 		}
+
+		//-------------------------------------------------
+		// texte principal
+		private static $indexCheminTexte = "action/DAO/indexTexteData.txt";
+
+		public static function lireTexteIndex() {
+			return file_get_contents(ContentDao::$indexCheminTexte);
+		}
+
+		public static function ecrireTexteIndex($message) {
+			file_put_contents(ContentDao::$indexCheminTexte, $message);
+		}
+
+		//-------------------------------------------------
+		// PAGE ÉQUIPE
+		//-------------------------------------------------
+
 	}
--- Web/action/IndexAction.php
+++ Web/action/IndexAction.php
@@ -18,17 +18,40 @@
 		public function optionsTitre() {
 
 			if (isset($_POST["texte-index-titre"])) {
-				ContentDao:: ecrire($_POST["texte-index-titre"]);
+				ContentDao:: ecrireTitreIndex($_POST["texte-index-titre"]);
 			}
 
 			$formulaireA = "<form id=\"ck-index-titre\" action=\"index\" method=\"post\">";
 			$zoneTexteA = "<textarea name=\"texte-index-titre\">";
-			$contenu = ContentDao::lire();
+			$contenu = ContentDao::lireTitreIndex();
 			$zoneTexteB = "</textarea>";
 			$cadreBoutonEnvoyerA = "<div class=\"cadre-bouton-envoyer\">";
 			$boutonEnvoyer = "<button class=\"bouton-envoyer\" type=\"submit\">Envoyer</button>";
 			$cadreBoutonEnvoyerB = "</div>";
 			$ckEditor = "<script>CKEDITOR.replace(\"texte-index-titre\", {height: \"102px\"});</script>";
+			$formulaireB = "</form >";
+
+			$html_1 = $formulaireA . $zoneTexteA . $contenu . $zoneTexteB;
+			$html_2 = $cadreBoutonEnvoyerA . $boutonEnvoyer . $cadreBoutonEnvoyerB;
+			$html_3 = $ckEditor . $formulaireB;
+
+			return $html_1 . $html_2 . $html_3;
+		}
+
+		public function optionsTexte() {
+
+			if (isset($_POST["texte-index-main"])) {
+				ContentDao:: ecrireTexteindex($_POST["texte-index-main"]);
+			}
+
+			$formulaireA = "<form id=\"ck-index-main\" action=\"index\" method=\"post\">";
+			$zoneTexteA = "<textarea name=\"texte-index-main\">";
+			$contenu = ContentDao::lireTexteIndex();
+			$zoneTexteB = "</textarea>";
+			$cadreBoutonEnvoyerA = "<div class=\"cadre-bouton-envoyer\">";
+			$boutonEnvoyer = "<button class=\"bouton-envoyer\" type=\"submit\">Envoyer</button>";
+			$cadreBoutonEnvoyerB = "</div>";
+			$ckEditor = "<script>CKEDITOR.replace(\"texte-index-main\", {height: \"550px\"});</script>";
 			$formulaireB = "</form >";
 
 			$html_1 = $formulaireA . $zoneTexteA . $contenu . $zoneTexteB;

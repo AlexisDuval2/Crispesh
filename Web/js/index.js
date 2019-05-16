df968bde542a910754f28858ec7a98d2857e5eb3
Duval Alexis <1806165@decinfo.cvm>
2019-05-16 09:16:45 -0400

    confirmation avec bouton envoyer

--- Web/js/index.js
+++ Web/js/index.js
@@ -1,5 +1,14 @@
 
-const toggleTitre = () => {
-	let boite1 = document.getElementById("ck-index-titre");
-	boite1.style.display = "none";
+window.onload = () => {
+	ecouterLesBoutonsEnvoyer();
+}
+
+const ecouterLesBoutonsEnvoyer = () => {
+	let message = "Vos modifications ont été enregistrées";
+	let boutons = document.getElementsByClassName("bouton-envoyer");
+
+	for (let i = 0; i < boutons.length; i++) {
+		let bouton = boutons[i];
+		bouton.onclick = function () { alert(message); }
+	}
 }

a229dc589e97dc3a774f3249046469c1aabcf132
Duval Alexis <1806165@decinfo.cvm>
2019-05-16 21:11:23 -0400

    animation fade-out bof!

--- Web/js/admin.js
+++ Web/js/admin.js
@@ -2,15 +2,16 @@
 let menuAdminEstVisible = false;
 let timerInterval = null;
 
-let bouton = document.getElementById("bouton-menu-admin");
-bouton.onclick = function () { menuAdmin(); }
+let menuAdmin = document.getElementById("menu-admin");
+
+let boutonMenuAdmin = document.getElementById("bouton-menu-admin");
+boutonMenuAdmin.onclick = function () { controlerMenuAdmin(); }
 
 //-----------------------------------------------
 // méthode pour afficher/masquer le menu admin
 //-----------------------------------------------
-const menuAdmin = () => {
+const controlerMenuAdmin = () => {
 
-	let menuAdmin = document.getElementById("menu-admin");
 
 	if (menuAdminEstVisible) {
 		menuAdminEstVisible = false;
@@ -19,10 +20,35 @@ const menuAdmin = () => {
 	else {
 		menuAdminEstVisible = true;
 		menuAdmin.style.display = "block";
-		setTimeout(temp, 1000);
+		// setTimeout(temp, 1000);
+		fadeOut();
 	}
 }
 
 function temp() {
 	alert("test");
 }
+
+
+
+
+
+var duration = 3000; /* fade duration in millisecond */
+var hidtime = 2000; /* time to stay hidden */
+var showtime = 2000; /* time to stay visible */
+
+function SetOpa(Opa) {
+	menuAdmin.style.opacity = Opa;
+
+
+	menuAdmin.style.MozOpacity = Opa;
+	menuAdmin.style.KhtmlOpacity = Opa;
+	menuAdmin.style.filter = 'alpha(opacity=' + (Opa * 100) + ');';
+  }
+
+  function fadeOut() {
+	for (i = 0; i <= 1; i += 0.01) {
+	  setTimeout("SetOpa(" + (1 - i) +")", i * duration);
+	}
+	//  setTimeout("fadeIn()", (duration + hidtime));
+  }

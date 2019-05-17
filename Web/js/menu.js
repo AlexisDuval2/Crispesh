b9d3013a854e813d201935ccea54f382b85e1370
Duval Alexis <1806165@decinfo.cvm>
2019-05-17 18:16:39 -0400

    animation menu

--- Web/js/menu.js
+++ Web/js/menu.js
@@ -1,5 +1,35 @@
 
-let boutonMenu = document.getElementById("mobile-menu-button");
-console.log(boutonMenu);
-boutonMenu.style.backgroundColor = "red";
+//-----------------------------------------------
+// constantes
+//-----------------------------------------------
+const idBoutonMenu = "mobile-menu-button";
+const couleurOff = "rgb(168, 225, 42)";
+const couleurOn = "rgb(11, 191, 242)";
+
+//-----------------------------------------------
+// variables
+//-----------------------------------------------
+let on = false;
+let boutonMenu = document.getElementById(idBoutonMenu);
+
+boutonMenu.style.backgroundColor = couleurOff;
 boutonMenu.style.border = "none";
+
+boutonMenu.onclick = function () { animerMenu(); }
+
+//-----------------------------------------------
+// méthode pour animer le menu mobile
+//-----------------------------------------------
+const animerMenu = () => {
+
+	if (!on) {
+		boutonMenu.style.backgroundColor = couleurOn;
+
+		on = true;
+	}
+	else {
+		boutonMenu.style.backgroundColor = couleurOff;
+
+		on = false;
+	}
+}

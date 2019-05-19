796e80e175f3868109c59ce9ab77de024cbccd8e
Duval Alexis <1806165@decinfo.cvm>
2019-05-18 20:03:41 -0400

    déboguage de la fonction générique afficherCkEditor()

--- Web/action/IndexAction.php
+++ Web/action/IndexAction.php
@@ -15,20 +15,10 @@
 		//-----------------------------------------------
 		// méthode pour insérer le html pour modifier le titre (admin)
 		//-----------------------------------------------
-		public function afficherCkEditor() {
-
-			$textareaName = "texte-index-titre";
-			$formId = "ck-index-titre";
-			$contenu = ContentDao::lireTitreIndex();
-			$hauteur = 102;
-
-			if (isset($_POST[$textareaName])) {
-				ContentDao:: ecrireTitreIndex($_POST[$textareaName]);
-			}
+		public function afficherCkEditor($textareaName, $formId, $contenu, $hauteur) {
 
 			$formulaireA = "<form id=\"" . $formId . "\" method=\"post\">";
 			$zoneTexteA = "<textarea name=\"" . $textareaName . "\">";
-			// ici il y a $contenu dans le html
 			$zoneTexteB = "</textarea>";
 			$cadreBoutonModifierA = "<div class=\"cadre-bouton-modifier\">";
 			$boutonModifier = "<button class=\"bouton-modifier\" type=\"submit\"><a href=\"javascript:;\">Modifier</a></button>";
@@ -48,25 +38,35 @@
 		//-----------------------------------------------
 		public function optionsTitre() {
 
-			if (isset($_POST["texte-index-titre"])) {
-				ContentDao:: ecrireTitreIndex($_POST["texte-index-titre"]);
+			$textareaName = "texte-index-titre";
+			$formId = "ck-index-titre";
+			$hauteur = 102;
+
+			if (isset($_POST[$textareaName])) {
+				ContentDao:: ecrireTitreIndex($_POST[$textareaName]);
 			}
 
-			$formulaireA = "<form id=\"ck-index-titre\" method=\"post\">";
-			$zoneTexteA = "<textarea name=\"texte-index-titre\">";
 			$contenu = ContentDao::lireTitreIndex();
-			$zoneTexteB = "</textarea>";
-			$cadreBoutonModifierA = "<div class=\"cadre-bouton-modifier\">";
-			$boutonModifier = "<button class=\"bouton-modifier\" type=\"submit\"><a href=\"javascript:;\">Modifier</a></button>";
-			$cadreBoutonModifierB = "</div>";
-			$ckEditor = "<script>CKEDITOR.replace(\"texte-index-titre\", {height: \"102px\"});</script>";
-			$formulaireB = "</form >";
 
-			$html_1 = $formulaireA . $zoneTexteA . $contenu . $zoneTexteB;
-			$html_2 = $cadreBoutonModifierA . $boutonModifier . $cadreBoutonModifierB;
-			$html_3 = $ckEditor . $formulaireB;
+			return $this->afficherCkEditor($textareaName, $formId, $contenu, $hauteur);
 
-			return $html_1 . $html_2 . $html_3;
+
+
+			// $formulaireA = "<form id=\"ck-index-titre\" method=\"post\">";
+			// $zoneTexteA = "<textarea name=\"texte-index-titre\">";
+			// $contenu = ContentDao::lireTitreIndex();
+			// $zoneTexteB = "</textarea>";
+			// $cadreBoutonModifierA = "<div class=\"cadre-bouton-modifier\">";
+			// $boutonModifier = "<button class=\"bouton-modifier\" type=\"submit\"><a href=\"javascript:;\">Modifier</a></button>";
+			// $cadreBoutonModifierB = "</div>";
+			// $ckEditor = "<script>CKEDITOR.replace(\"texte-index-titre\", {height: \"102px\"});</script>";
+			// $formulaireB = "</form >";
+
+			// $html_1 = $formulaireA . $zoneTexteA . $contenu . $zoneTexteB;
+			// $html_2 = $cadreBoutonModifierA . $boutonModifier . $cadreBoutonModifierB;
+			// $html_3 = $ckEditor . $formulaireB;
+
+			// return $html_1 . $html_2 . $html_3;
 		}
 
 		//-----------------------------------------------
